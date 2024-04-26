e7ab007af8f4c8850f1cc5ff582e067e274c4e0b
murillodejesus <murillodejesus02@gmail.com>
2024-04-25 22:23:36 -0300

    update

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine("Now i'll test my knowledge on C#");
+
+        Console.Write("What is your First Name?");
+        string firstname = Console.ReadLine();
+        Console.Write("What is your Last Name?");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your Name is {lastname}, {firstname} {lastname}.");
     }
 }
