69731ff16f5fdcf3b5442607fc14067c6c5895dc
murillodejesus <murillodejesus02@gmail.com>
2024-05-02 19:51:40 -0300

    update

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,24 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create jobs
+        Job job1 = new Job("Microsoft", "Software Engineer", 2019, 2022);
+        Job job2 = new Job("Apple", "Manager", 2022, 2023);
+
+        // Display company of each job
+        job1.Display();
+        job2.Display();
+        
+        // Create a resume
+        Resume myResume = new Resume("Murillo de Jesus");
+        
+        // Add jobs to the resume
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        // Display resume
+        myResume.Display();
     }
 }
