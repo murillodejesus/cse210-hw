7e819283d38eb96e530258a64c6f2fbb5644d4a0
murillodejesus <murillodejesus02@gmail.com>
2024-05-05 00:49:23 -0300

    update

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -53,3 +53,32 @@ class Journal
     }
 }
 
+class Program
+{
+    static void Main (string[] args)
+    {
+        Journal journal = new Journal();
+
+        string[] prompts = {
+            "Who was the most interesting person I interacted with today?",
+            "What was the best part of my day?",
+            "How did I see the hand of the Lord in my life today?",
+            "What was the strongest emotion I felt today?",
+            "If I had one thing I could do over today, what would it be?"
+        };
+
+        Random rand = new Random ();
+        bool exit = false;
+        while (!exit)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the Journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Exit");
+
+        }
+    }
+}
+
