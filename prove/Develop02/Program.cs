256965e6f4541db1bbf80bfebb9042f05434790c
murillodejesus <murillodejesus02@gmail.com>
2024-05-05 01:05:21 -0300

    update

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -115,7 +115,7 @@ class Program
                 case 4:
                     Console.Write("Enter filename to load: ");
                     string loadFilename = Console.ReadLine();
-                    journal.LoadFromFile(loadFilename);
+                    journal.SaveToFile(loadFilename);
                     Console.WriteLine("Journal loaded successfully.");
                     break;
                 case 5:
