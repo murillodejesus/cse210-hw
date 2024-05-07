efc83575ac790a4365963ae9ece4732928459064
murillodejesus <murillodejesus02@gmail.com>
2024-05-07 06:44:23 -0300

    update

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,35 +1,26 @@
 using System;
 public class Fraction
 {
-    private int _top;
-    private int _bottom;
+    private int numerator;
+    private int denominator;
 
     public Fraction()
     {
-        _top = 1;
-        _bottom = 1;
+        numerator = 1;
+        denominator = 1;
     }
 
     public Fraction(int wholeNumber)
     {
-        _top = wholeNumber;
-        _bottom = 1;
+        numerator = wholeNumber;
+        denominator = 1;
     }
 
-    public Fraction (int top, int bottom)
+    public Fraction (int numerator, int denominator)
     {
-        _top = top;
-        _bottom = bottom;
+        this.numerator = numerator;
+        this.denominator = denominator;
     }
 
-    public string GetFractionString()
-    {
-        string text = $"{_top}/{_bottom}";
-        return text;
-    }
-
-    public double GetDecimalValue()
-    {
-        return (double)_top / (double)_bottom;
-    }
+    
 }
