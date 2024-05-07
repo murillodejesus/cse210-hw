8826582a29f194adaa210bd98d9f0cc57a8fe472
murillodejesus <murillodejesus02@gmail.com>
2024-05-07 06:49:21 -0300

    update

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -22,5 +22,32 @@ public class Fraction
         this.denominator = denominator;
     }
 
-    
+    public int Numerator
+    {
+        get { return numerator; }
+        get { numerator = value; }
+    }
+
+    public int Denominator
+    {
+        get { return denominator; }
+        set
+        {
+            if (value != 0)
+                denominator = value;
+            else 
+                throw new ArgumentException("Denominator cannot be zero.");
+        }
+    }
+
+    public string GetFractionString()
+    {
+        return $"{numerator}/{denominator}";
+    }
+
+    public double GetDecimalValue();
+    {
+        return (double) numerator / denominator;
+    }
+
 }
