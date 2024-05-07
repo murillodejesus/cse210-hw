63818d40ece5768993beaf148ffbc890b4b385c9
murillodejesus <murillodejesus02@gmail.com>
2024-05-07 06:52:47 -0300

    update

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -7,13 +7,16 @@ class Program
     {
        Fraction fraction1 = new Fraction();
        Fraction fraction2 = new Fraction(5);
-       Fraction Fraction3 = new Fraction(6,7);
+       Fraction fraction3 = new Fraction(6,7);
 
        Console.WriteLine(fraction1.GetFractionString());
        Console.WriteLine(fraction1.GetDecimalValue());
 
-       Console.WriteLine(fraction1.GetFractionString());
-       Console.WriteLine(fraction1.GetDecimalValue());
+       Console.WriteLine(fraction2.GetFractionString());
+       Console.WriteLine(fraction2.GetDecimalValue());
+
+       Console.WriteLine(fraction3.GetFractionString());
+       Console.WriteLine(fraction3.GetDecimalValue());
 
     
  
